eedf5f5460a4300ce58972d19427f045fd185e51
willi16 <willifrancis39@gmail.com>
2026-08-05 18:10:46 +0000

    Fix : servir les médias via Django quel que soit DEBUG (static() est un no-op hors debug en Django 5.2) - logo/fond/cachet affichés dans l'aperçu facture et le login

--- devia_gestion/deiva_gestion/urls.py
+++ devia_gestion/deiva_gestion/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, re_path, include
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from gestion_depot.forms import LoginForm
 
 
@@ -36,7 +36,8 @@ urlpatterns = [
     path('gestion/', include('gestion_depot.urls')), 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
 
